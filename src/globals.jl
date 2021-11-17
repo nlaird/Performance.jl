6ff9c894205646024b18d2475670425664c5173f
Nicolette Laird <n.laird@wustl.edu>
2021-11-17 13:53:02 -0600

    Changed globals to use Ref correctly

--- src/globals.jl
+++ src/globals.jl
@@ -5,10 +5,10 @@ charset = 'a':'z'
 getchar(idx::Int64) = charset[idx]::Char
 
 
-homedir = "wrong"
+const homedir = Ref{String}("wrong")
 
 function set_homedir(path)
-    global homedir = path
+    global homedir[] = path
 end
-get_homedir() = Ref{String}(homedir)[]
+get_homedir() = homedir[]
 
