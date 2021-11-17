d71ab52a119f313bcd7a4665f9e1691a62484512
Nicolette Laird <n.laird@wustl.edu>
2021-11-17 10:21:34 -0600

    hopefully this works

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,11 +1,7 @@
 using Performance
 using Test
 
-<<<<<<< HEAD
-ntests_finished = 2   # increment this counter to run the next set of tests
-=======
 ntests_finished = 1   # increment this counter to run the next set of tests
->>>>>>> 84ad0e0f34cda22b5d5dd205678d9a1dc15735cf
 
 @testset "Performance.jl" begin
     ntests = ntests_finished
