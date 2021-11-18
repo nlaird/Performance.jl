34c9c4d403b8f0f863071be5030a2f4e9b36e466
Nicolette Laird <n.laird@wustl.edu>
2021-11-18 11:51:59 -0600

    Finish array tests

--- src/arrays.jl
+++ src/arrays.jl
@@ -1,13 +1,13 @@
 export sequence_of_three, alternate_items
 
-function sequence_of_three(start::Union{Float64, Int64})
+function sequence_of_three(start)
     out = typeof(start)[]
     push!(out, start, start+1, start+2)
     return out
 end
 
 function alternate_items(nrepeats::Integer, item1, item2)
-    out = Integer[]
+    out = Vector{Union{typeof(item1), typeof(item2)}}()
     for _ = 1:nrepeats           # _ is often used for a variable you'll not use again
         push!(out, item1, item2)
     end
