66c9e0d03a702fce5ae52e7fa199174d730db512
Nicolette Laird <n.laird@wustl.edu>
2021-11-19 15:30:38 -0600

    struct tests all pass!

--- src/structs.jl
+++ src/structs.jl
@@ -5,7 +5,8 @@ struct DefaultArray{T, N} <: AbstractArray{T, N}
     parentarray::Array{T,N}
     defaultvalue::T
 end
-DefaultArray(parentarray, defaultvalue) = DefaultArray{eltype(parentarray), ndims(parentarray)}(parentarray, defaultvalue)
+DefaultArray(parentarray, defaultvalue) = DefaultArray{promote_type(eltype(parentarray), typeof(defaultvalue)), ndims(parentarray)}(parentarray, defaultvalue)
+DefaultArray(parentarray, nothing) = DefaultArray{Union{eltype(parentarray), Nothing}, ndimsndims(parentarray)}(parentarray, nothing)
 
 @inline Base.getindex(a::DefaultArray{T, N}, i::Vararg{Int,N}) where {T,N} = checkbounds(Bool, a, i...) ? a.parentarray[i...] : a.defaultvalue
 Base.size(a::DefaultArray) = size(a.parentarray)
