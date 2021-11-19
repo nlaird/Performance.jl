84ef8a2a953afb8d1bae9ab0d6e7533895c31794
Nicolette Laird <n.laird@wustl.edu>
2021-11-19 15:22:33 -0600

    Fix for first part of structs tests

--- src/structs.jl
+++ src/structs.jl
@@ -1,11 +1,11 @@
 export DefaultArray
 
 # To solve this problem, you'll have to improve the `struct` definition and modify/add constructors
-struct DefaultArray{N} <: AbstractArray{Any,N}
-    parentarray::AbstractArray
-    defaultvalue
+struct DefaultArray{T, N} <: AbstractArray{T, N}
+    parentarray::Array{T,N}
+    defaultvalue::T
 end
-DefaultArray(parentarray, defaultvalue) = DefaultArray{ndims(parentarray)}(parentarray, defaultvalue)
+DefaultArray(parentarray, defaultvalue) = DefaultArray{eltype(parentarray), ndims(parentarray)}(parentarray, defaultvalue)
 
-@inline Base.getindex(a::DefaultArray{N}, i::Vararg{Int,N}) where N = checkbounds(Bool, a, i...) ? a.parentarray[i...] : a.defaultvalue
+@inline Base.getindex(a::DefaultArray{T, N}, i::Vararg{Int,N}) where {T,N} = checkbounds(Bool, a, i...) ? a.parentarray[i...] : a.defaultvalue
 Base.size(a::DefaultArray) = size(a.parentarray)
