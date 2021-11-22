53cf51b00750dd92ab8ea1ec3a34116825f70018
Nicolette Laird <n.laird@wustl.edu>
2021-11-21 18:38:31 -0600

    Finish stuff for algorithm_order

--- test/algorithm_order.jl
+++ test/algorithm_order.jl
@@ -17,12 +17,24 @@ using Test
     end
 
     d2 = Dict{Char,Int}()
-    t2 = @elapsed # ...
+    t2 = @elapsed for c in str
+        d2[c] = get(d2, c, 0) + 1
+    end
     @test t2 < t
 
     # Since it's a discrete list of values, can you do even better with an array?
     # Hint: try `Int('z')` for ideas.
     counter = zeros(Int, 26*2+10)
-    t3 = @elapsed # ...
+
+    t3 = @elapsed for c in str
+        if Int(c) > 96
+            idx = (c-'a')+1
+        elseif Int(c) > 64
+            idx = (c-'A') + 27
+        else
+            idx = (c-'0') + 53
+        end
+        counter[idx]+=1
+    end
     @test t3 < t2
 end
